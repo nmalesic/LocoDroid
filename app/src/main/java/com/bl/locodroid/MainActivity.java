82bbb04227cdbf4c5652429276dd13d4003fb0ee
sylvain.rabois <sylvain.rabois@berger-levrault.fr>
2016-02-05 15:17:01 +0100

    modifs sylvain 1

---
+++ app/src/main/java/com/bl/locodroid/MainActivity.java
@@ -0,0 +1,33 @@
+package com.bl.locodroid;
+
+/**
+ * Created by SRABOIS on 05/02/2016.
+ */
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Log.i("EPITEZ", "Created activity 1");
+
+        Button but_next = (Button)findViewById(R.id.Button01);
+        but_next.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+
+                //get context() pour récupérer la vue, puis on renseigne la classe vers laquelle on veut switcher
+                Intent myIntent = new Intent(view.getContext(), MapsActivity.class);
+                startActivityForResult(myIntent, 0);
+            }
+        });
+
+    }
+}
