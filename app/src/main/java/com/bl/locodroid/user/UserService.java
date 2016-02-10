0f8016ba5a52d145a9f39657b519aafe083a745c
sylvain.rabois <sylvain.rabois@berger-levrault.fr>
2016-02-10 12:00:59 +0100

    user and address all in english

---
+++ app/src/main/java/com/bl/locodroid/user/UserService.java
@@ -0,0 +1,44 @@
+package com.bl.locodroid.user;
+
+/**
+ * Created by SRABOIS on 09/02/2016 apres l'apero et avant les pizzas
+ */
+public class UserService implements  IUserService {
+
+    private User user;
+
+    @Override
+    public User getLocalUser() {
+        return null;
+    }
+
+    @Override
+    public boolean setLocalUser(User user) {
+        return false;
+    }
+
+    @Override
+    public User connect(String login, String password) {
+        return null;
+    }
+
+    @Override
+    public boolean disconnect() {
+        return false;
+    }
+
+    @Override
+    public boolean subscribeUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean updateUser(User user) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteUser(User user) {
+        return false;
+    }
+}
