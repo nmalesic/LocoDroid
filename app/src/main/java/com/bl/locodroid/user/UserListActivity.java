d6cf59cd98ad90a1bb7a8400a36c837aeaf2a1ba
sylvain.rabois <sylvain.rabois@berger-levrault.fr>
2016-02-29 11:19:08 +0100

    correction userlist

--- app/src/main/java/com/bl/locodroid/user/UserListActivity.java
+++ app/src/main/java/com/bl/locodroid/user/UserListActivity.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -130,6 +131,9 @@ public class UserListActivity extends MenuActivity {
                 startActivityForResult(myIntent, 0);
             }
         });
+
+        TextView nb = (TextView) findViewById(R.id.user_nb);
+        nb.setText("Nombre de voisins trouvées : ");
     }
 
 
